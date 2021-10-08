9cc2ad1822baa55c061ec56aa73bc310027fa1a6
Kirill <14krabik14@gmail.com>
2021-10-09 00:50:32 +0300

    edit title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>City manager</title>
     <link rel="stylesheet" href="css/null.css">
     <link rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
